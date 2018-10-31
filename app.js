0d96862e4139ffc379bbf871b247c85cdbd32a82
Carlos Castro Martos <44461162+ccastromar@users.noreply.github.com>
2018-10-31 11:55:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let express = require('express');
+const path = require('path');
+
+let app = express();
+const index = require('./routes/index');
+
+
+let i18n = require("i18n");
+i18n.configure({
+   locales:['en', 'es'],
+   queryParameter: 'lang',
+   defaultLocale: 'en',
+   updateFiles: false,
+   syncFiles: false,
+   cookie: 'i18n',
+   directory: __dirname + '/locales'
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(i18n.init);
+
+app.use(express.static('public'));
+
+app.use('/', index);
+
+
+let server = app.listen(8000, function () {
+
+   console.log('Express app listening at port 8000');
+
+});
